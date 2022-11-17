3f04aebf78df356aaf844ec836dc81223a684dda
htp06152000 <90561047+htp06152000@users.noreply.github.com>
2022-11-17 23:52:59 +0800

    LATEST FOR NOV17

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,3 +1,7 @@
 <?php if ( ! defined('ACCESS') ) die("Direct access not allowed."); ?>
 
-
+<div class="row justify-content-center vh-100">
+    <div class="welcome">
+        <h1 class="text-primary bg-light" style="margin-top: 250px; width: 450px">Welcome to Dashboard</h1>
+    </div>
+</div>
